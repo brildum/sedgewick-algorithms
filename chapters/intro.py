e9f5601b3e37efae767d1cda9e5ce8d616f0af2f
Chris Carroll <brildum@gmail.com>
2011-07-30 10:58:27 -0400

    Added intro algorithms

---
+++ chapters/intro.py
@@ -0,0 +1,17 @@
+
+def gcd(u, v):
+    """
+    Returns the greatest common denominator (or greatest common factor) of
+    u and v.
+    """
+    if v == 0:
+        return u
+    return gcd(v, u%v)
+
+def lowest_terms(numerator, denominator):
+    """
+    Given a numerator and denominator (representing a fraction), returns a tuple
+    of the fraction in its lowest terms.
+    """
+    d = gcd(numerator, denominator)
+    return (numerator/d, denominator/d)
